28f9a92f7687e430071f2d74b96479169cacfd56
Elena Cirstea <elender1230@gmail.com>
2020-02-07 10:35:20 +0100

    Revert "stop tracking"
    This reverts commit a091335c8972db49ceea257c74320c5ee83c4ac8.

---
+++ web/config/config.php
@@ -0,0 +1,10 @@
+<?php
+namespace Config;
+//constants for the DataBase
+
+// **********************SHOULD BE CHANGED ACCORDING TO THE HOST***********************************
+define ('URL','http://localhost/ControlFichajes/web');
+const DSN = 'mysql:dbname=fingerprintassistancecontrol;host=127.0.0.1';
+const USER = 'root';
+const PASSWORD = '';
+?>
