716b7518abf6c8d201d00278968db5f5b9afd80a
Daniel Gomez <daniel.gomez@movicoders.com>
2020-02-11 10:55:09 +0100

    python connection success with remote DB from Raspberry

--- raspberry/DB/connection.py
+++ raspberry/DB/connection.py
@@ -1,20 +1,26 @@
 import mysql.connector
 
 class SQLConnect():
-    def __init__(self, controller):
+    def __init__(self):
 
-        self.controller = controller
+        #self.controller = controller
         self.dbconfig = {'host': "192.168.2.75",
-                    'user': "root",
-                    'password': "",
-                    'database': 'fingerprintassistancecontrol',
+                    'user': "root2",
+                    'password': "root2",
+                    'database': "fingerprintassistancecontrol",
                     'port': '3306',
-                    'unix_socket': '/run/mysqld/mysqld.sock'
                     }
         self.conn = mysql.connector.connect(**self.dbconfig)
         self.cursor = self.conn.cursor()
+        #_SQL = """SHOW tables"""
+        #self.cursor.execute(_SQL)
+        #res = self.cursor.fetchall()
+        #print(res)
 
 
-    def find_user_by_FP(self, template_number):
-        _SQL = """SELECT employeeDni FROM users WHERE fingerprint = template_number;"""
+    def find_user_by_FP(self):
+        _SQL = "SHOW tables"
         return self.cursor.execute(_SQL)
+        
+
+conecc = SQLConnect()
