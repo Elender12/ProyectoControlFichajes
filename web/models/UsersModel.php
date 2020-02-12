6225ceb10e9a6ae57fd9888da135be45f49c15ce
Mihai <mihai.aurar@movicoders.com>
2020-02-12 11:58:38 +0100

    Fix userModel

--- web/models/UsersModel.php
+++ web/models/UsersModel.php
@@ -144,7 +144,7 @@ class UsersModel extends Model
     {
         require "views/main/index.php";
     }
-
+//Prueba
     public function testCharts(){
         require "views\charts\EmployeeStatisticsPage.php";
     }
