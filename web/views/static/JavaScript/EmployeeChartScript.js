e5f421b39a9167aa8dd017f26855d7a4009b41e1
Elena Cirstea <elender1230@gmail.com>
2020-02-21 12:47:43 +0100

    update statistics

--- web/views/static/JavaScript/EmployeeChartScript.js
+++ web/views/static/JavaScript/EmployeeChartScript.js
@@ -2,35 +2,6 @@ google.charts.load('current', {packages: ['corechart']});
 google.setOnLoadCallback(drawChart);
 
 function drawChart() {
-
-
-  // var data = google.visualization.arrayToDataTable([
-  //   ['Day of the month', 'Hours by contract', 'Worked hours'],
-  //   ['02/01', 8, 3.5],
-  //   ['03/01', 8, 4],
-  //   ['07/01', 8, 8],
-  //   ['03/01', 8, 4],
-  //   ['03/01', 8, 6],
-  //   ['03/01', 8, 7.5],
-  //   ['03/01', 8, 4],
-  //   ['05/01', 8, 7],
-  //   ['03/01', 8, 5],
-  //   ['03/01', 8, 4],
-  //   ['03/01', 8, 3],
-  //   ['03/01', 8, 8],
-  //   ['09/01', 8, 6],
-  //   ['03/01', 8, 4],
-  //   ['03/01', 8, 6.5],
-  //   ['03/01', 8, 4],
-  //   ['03/01', 8, 4],
-  //   ['15/01', 8, 3.2],
-  //   ['03/01', 8, 4],
-  //   ['03/01', 8, 4.5],
-  //   ['03/01', 8, 4],
-  //   ['03/01', 8, 2],
-  //   ['03/01', 8, 0]
-
-  // ]);
   var jsonData = $.ajax({
     url: "sendData",
     dataType: "json",
@@ -44,25 +15,15 @@ function drawChart() {
   //   }).done( function(respuesta){
   //       console.log(respuesta);
   //   });
-
-
   var options = {
     title: 'Worked hours',
     hAxis: {title: 'Working days'},
     vAxis: {title: 'Worked hours', minValue: 0},
     legend: { position: 'bottom'}
   };
-
-  //console.log("entra en draw");
-  var data = new google.visualization.DataTable(jsonData);
-  var chart = new google.visualization.AreaChart(document.getElementById('employeeChart_div'));
-  chart.draw(data, options);
- // chart.draw(data, {width: 400, height: 240});
-
- 
+   var data = new google.visualization.DataTable(jsonData);
+   //var chart = new google.visualization.AreaChart(document.getElementById('employeeChart_div'));
+    var chart = new google.visualization.ColumnChart(document.getElementById('employeeChart_div'));
+   chart.draw(data, options);
 }
 
-// $(window).resize(function(){
-//   drawChart();
- 
-// });
