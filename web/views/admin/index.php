3c57926afb2c4875c9438d1e011d3e05be5456fe
Mihai <mihai.aurar@movicoders.com>
2020-02-26 11:13:50 +0100

    User y Admin Fixed

--- web/views/admin/index.php
+++ web/views/admin/index.php
@@ -16,15 +16,13 @@
     <!-- MI CSS -->
 	<link rel="stylesheet" href="../views/static/css/user.css">
 	
-
     <!-- La Scrollbar con CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
 
     <!-- LA Fuente Awesome JS -->
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
-    
-    
+
        <!-- Los iconos tipo Solid de Fontawesome los importo y los utilizo mas adelante-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
     <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
@@ -33,7 +31,6 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/js/tempusdominus-bootstrap-4.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/css/tempusdominus-bootstrap-4.min.css" />
-
 </head>
 <body>
   <!-- php sessions START -->
@@ -43,57 +40,27 @@
         <!-- Sidebar Con todas sus partes  -->
         <nav id="sidebar">
             <div class="sidebar-header">
-               
                 <h3>Movicoders</h3>
-              
             </div>
-
             <ul class="list-unstyled components">
                <!-- <p><i class="fas fa-chevron-circle-down"></i>  Menu</p>-->
                <li>
                 <a class="d-block d-sm-none d-sm-block d-md-none d-md-block d-lg-none"a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" >
                 <i class="fas fa-mobile-alt"></i> Phone</i></a>
                 <ul class="collapse list-unstyled" id="pageSubmenu">
-
                   <li class="nav-item active"> <!--User-->
                     <a class="nav-link d-block d-sm-none d-sm-block d-md-none d-md-block d-lg-none" href="#"><i class="fas fa-user"></i> User</a>
                 </li>
-                
-                <li class="nav-item"> <!--Home-->
-                  <a class="nav-link d-block d-sm-none d-sm-block d-md-none d-md-block d-lg-none" href="https://movicoders.com/"  target="_blank" ><i class="fas fa-home"></i> Home</a>
-              </li>
-                <li class="nav-item"  > <!--CONFIG -->
-                  <a class="nav-link  d-block d-sm-none d-sm-block d-md-none d-md-block d-lg-none"data-toggle="modal" 
-                                        data-target=".bd-example-modal-lg">
-                                        <i class="fas fa-user-cog "></i> Config</a> 
-
-                  <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-lg">
-                      <div class="modal-content">
-                        ..............
-                      </div>
-                    </div>
-                  </div>
-              </li>
                 <li class="nav-item"><!--Salida-->
                   <a class="nav-link   d-block d-sm-none d-sm-block d-md-none d-md-block d-lg-none" href="../Html/index."><i class="fas fa-sign-out-alt"></i> Exit</a>
-              </li>
-                  
+              </li>  
                 </ul>
             </li>
-          
-            
-          
-        
         <li>
 		<a href="https://movicoders.com/contact/" target="_blank"> <i class="fas fa-inbox"></i> Contact </a>
-
         </li>
         </ul>    
-              
-
-    </nav>
-            
+    </nav>       
         </nav>
         <!-- El coment de la pagina  -->
         <div id="content">
@@ -106,11 +73,6 @@
 					<div class="topnav-centered2 d-none d-xl-block d-lg-block">
     				<a href="#" class="active ">Admin Panel</a>
   							</div>
-    
-                    
-                  <!--  <h5 class="modal-title" id="cnt-fichaje"> Control de Fichajes </h5>-->
-                    
-                    <!--La Barra donde aparecen los diferentes iconos que podemos ver en pantalla -->
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
 						
                         <ul class="nav navbar-nav ml-auto  "> 
@@ -120,25 +82,8 @@
                             <li class=" nav-item active"> <!--User-->
                                 <a class="nav-link" href="#"><i class="fas fa-user " id="userid"></i></i></a>
                             </li>
-
-                            <li class="nav-item"> <!--Home-->
-                                <a class="nav-link" href="https://movicoders.com/"  target="_blank"><i class="fas fa-home" id="homeid"></i> </a>
-                            </li>
-
-                            <li class="nav-item"  > <!--CONFIG -->
-                                <a class="nav-link" data-toggle="modal" id="btn-config"data-target=".bd-example-modal-lg"><i class="fas fa-user-cog "></i> </a> 
-								<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                                  <div class="modal-dialog modal-lg">
-                                    <div class="modal-content">
-                                      hola
-                                    </div>
-                                  </div>
-                                </div>
-                            </li>
-
                             <li class="nav-item"><!--Salida-->
                                 <a class="nav-link" href="exit"><i class="fas fa-sign-out-alt" id="exitid"></i> </a>
-                                
                             </li>
                         </ul>
                     </div>
@@ -175,33 +120,6 @@
               </div>
 
             <div class="line"></div>
-
-            
-<!--EL TEMA DE CAMBIAR DE COLOR ES ALGO QUE SOLO ESTOY PROBANDO AUN NO ESTA LISTO Y
-CREO QUE NO SE LLEGARA A IMPLEMENTAR EN LA PARTE FINAL 
-            <div class="line"></div>
-			<body class="bg-white text-center d-flex h-100">
-  		<div class="container d-flex p-3 mx-auto flex-column">
-    <header class="mb-auto">
-      <h3 class="float-left">Dark Mode Switch</h3>
-      <nav class="nav justify-content-center float-right">
-      
-        <div class="nav-link">
-
-          <div class="custom-control custom-switch">
-            <input type="checkbox" class="custom-control-input" id="darkSwitch">
-            <label class="custom-control-label" for="darkSwitch">Dark Mode</label>
-          </div>
-
-            <h3></h3>
-            <p></p>
-            
-        </div>
-    </div>
-
-    --->
-  <!--<p>Date: <input type="text" id="fromDate"> TO <input type="text" id="toDate"></p>-->
-
     <!-- jQuery CDN -  AJAX -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <!-- Popper.JS -->
