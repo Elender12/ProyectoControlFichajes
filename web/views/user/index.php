956980bb672f1c2409a2a9bd30433b7383770724
Elena Cirstea <elender1230@gmail.com>
2020-03-02 12:13:24 +0100

    fix

--- web/views/user/index.php
+++ web/views/user/index.php
@@ -133,13 +133,8 @@
                   }
                 
                  ?></br></h6>
-<<<<<<< HEAD
 				 
 				 	  <?php 
-=======
-				 <br>
-				 <i class="fas fa-user-tie"></i> <?php 
->>>>>>> dd798a262a15cda33dfe358789d873ea4f198061
       //  if(isset($_SESSION["workerNAME"])){
       //   echo $_SESSION["workerNAME"];
       //   echo $_SESSION["NOMBRE"];
